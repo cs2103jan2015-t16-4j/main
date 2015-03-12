3fefdd562201f4e7c53fa69d36de0edeab63e014
Lin Zirui <noctislin@gmail.com>
2015-03-12 12:29:13 +0800

    Add invalid message output.

--- src/Display.java
+++ src/Display.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 
 public class Display {
-	
-	private static final String MESSAGE_WELCOME="Welcome to AnyTask.\n";
-	private static final String MESSAGE_ADD="Added %s \n";
-	private static final String MESSAGE_DELETELINE="Deleted %s \n";
-	private static final String MESSAGE_DISPLAY_SUCCESS="%d. %s \n";
-	private static final String MESSAGE_DISPLAY_EMPTY="There are no tasks to display \n";
-	private static final String MESSAGE_PROMPT="Command:";
-	private static final String MESSAGE_ERROR="ERROR: \n";
 
-	private static String output ="";
-	
-	private static void print(){
+	private static final String MESSAGE_WELCOME = "Welcome to AnyTask.\n";
+	private static final String MESSAGE_ADD = "Added %s \n";
+	private static final String MESSAGE_DELETELINE = "Deleted %s \n";
+	private static final String MESSAGE_DISPLAY_SUCCESS = "%d. %s \n";
+	private static final String MESSAGE_DISPLAY_EMPTY = "There are no tasks to display \n";
+	private static final String MESSAGE_DISPLAY_INVALID = "Command invalid \n";
+	private static final String MESSAGE_PROMPT = "Command:";
+	private static final String MESSAGE_ERROR = "ERROR: \n";
+
+	private static String output = "";
+
+	private static void print() {
 		System.out.printf(output);
 	}
 
@@ -21,43 +22,47 @@ public class Display {
 		print();
 	}
 
-	public static void displayMsgAdd(String userText){
-    	output = String.format(MESSAGE_ADD,userText);
-    	print();
-    }
-    
-	public static void displayMsgDeleteLine(String line){
-    	output = String.format(MESSAGE_DELETELINE, line);
-    	print();
-    }
-    
-	public static void displayMsgDisplay(int i,String line){
-    	output = String.format(MESSAGE_DISPLAY_SUCCESS, i, line);
-    	print();
-    }
-    
-	public static void displayMsgEmpty(){
-    	output = String.format(MESSAGE_DISPLAY_EMPTY);
-    	print();
-    }
-    
-	public static void displayMsgPrompt(){
-    	output = String.format(MESSAGE_PROMPT);
-    	print();
-    }
-	
-	public static void displayAll(){
-		ArrayList<Task> taskList=Database.getTaskList();
-		int i=1;
-		
+	public static void displayMsgAdd(String userText) {
+		output = String.format(MESSAGE_ADD, userText);
+		print();
+	}
+
+	public static void displayMsgDeleteLine(String line) {
+		output = String.format(MESSAGE_DELETELINE, line);
+		print();
+	}
+
+	public static void displayMsgDisplay(int i, String line) {
+		output = String.format(MESSAGE_DISPLAY_SUCCESS, i, line);
+		print();
+	}
+
+	public static void displayMsgEmpty() {
+		output = String.format(MESSAGE_DISPLAY_EMPTY);
+		print();
+	}
+
+	public static void displayMsgPrompt() {
+		output = String.format(MESSAGE_PROMPT);
+		print();
+	}
+
+	public static void displayAll() {
+		ArrayList<Task> taskList = Database.getTaskList();
+		int i = 1;
+
 		if (taskList.isEmpty()) {
-		    displayMsgEmpty();
+			displayMsgEmpty();
+		} else {
+			for (Task task : taskList) {
+				displayMsgDisplay(i, task.getName());
+				i++;
+			}
 		}
-		else{
-			for(Task task: taskList) {
-				displayMsgDisplay(i,task.getName());
-		    }
-			i++;
-		}
+	}
+
+	public static void displayMsgInvalid() {
+		output = String.format(MESSAGE_DISPLAY_INVALID);
+		print();
 	}
 }

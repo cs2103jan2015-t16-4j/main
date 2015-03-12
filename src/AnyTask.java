1ec9854d2f4d8c5588da7695e10206f0f3a5f249
Lin Zirui <noctislin@gmail.com>
2015-03-12 11:36:21 +0800

    Initialize with default file.

--- src/AnyTask.java
+++ src/AnyTask.java
@@ -1,55 +1,54 @@
 import java.util.Scanner;
 
 public class AnyTask {
-	private static final String OPERATION_ADD="add";
-	private static final String OPERATION_DISPLAY="display";
-	private static final String OPERATION_EDIT="edit";
-	private static final String OPERATION_DELETE="delete";
-	private static final String OPERATION_EXIT="exit";
-	private static final String OPERATION_SAVE="save";
+	static enum COMMAND_TYPE {
+		ADD, DISPLAY, EDIT, DELETE, SEARCH, SAVE, INVALID, EXIT
+	};
+	private static final String OPERATION_ADD = "add";
+	private static final String OPERATION_DISPLAY = "display";
+	private static final String OPERATION_EDIT = "edit";
+	private static final String OPERATION_DELETE = "delete";
+	private static final String OPERATION_EXIT = "exit";
+	private static final String OPERATION_SAVE = "save";
+
+	private static boolean isCorrectFormat = false;
+
+	private static Scanner sc = new Scanner(System.in);
 	
-	private static boolean isCorrectFormat= false;
-	
-	private static Scanner sc= new Scanner(System.in);	
-	
-	public static void executeUserCommand(String userCommand){
-		if (userCommand.equals(OPERATION_ADD)){
-			String userText=sc.nextLine();
-			Database.addTask(userText); 
-		}
-		else if (userCommand.equals(OPERATION_DISPLAY)){
-			Display.displayAll(); 
-		}
-		else if (userCommand.equals(OPERATION_EDIT)){
-			String oldName=sc.nextLine();
-			String newName=sc.nextLine();
-			Database.editTask(oldName,newName); 
-		}
-		else if (userCommand.equals(OPERATION_DELETE)){
-			String userText=sc.nextLine();
-			Database.deleteTask(userText); 
-		}
-		else if (userCommand.equals(OPERATION_SAVE)){
+	public static void executeUserCommand(String userCommand) {
+		
+		if (userCommand.equals(OPERATION_ADD)) {
+			String userText = sc.nextLine();
+			Database.addTask(userText);
+		} else if (userCommand.equals(OPERATION_DISPLAY)) {
+			Display.displayAll();
+		} else if (userCommand.equals(OPERATION_EDIT)) {
+			String oldName = sc.nextLine();
+			String newName = sc.nextLine();
+			Database.editTask(oldName, newName);
+		} else if (userCommand.equals(OPERATION_DELETE)) {
+			String userText = sc.nextLine();
+			Database.deleteTask(userText);
+		} else if (userCommand.equals(OPERATION_SAVE)) {
 			Database.saveTasksToFile();
-		}
-		else if (userCommand.equals(OPERATION_EXIT)){
+		} else if (userCommand.equals(OPERATION_EXIT)) {
 			System.exit(0);
+		} else {
+			throw new Error("Unrecognized command type");
 		}
-		else{
-			//error
-		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		if(args.length==0) {
-			isCorrectFormat=true;
+		if (args.length == 0) {
+			isCorrectFormat = true;
 			Display.displayMsgWelcome();
-		} 
-		else {
+		} else {
 			System.exit(0);
 		}
 		
+		Database.fetchTasksFromFile();
+
 		while (isCorrectFormat) {
 			Display.displayMsgPrompt();
 			String command = sc.next();

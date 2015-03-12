223e9635d03dd7493d317b6e36edc1c0c012b004
Lin Zirui <noctislin@gmail.com>
2015-03-12 10:56:41 +0800

    Add description-related method in Task class.

--- src/Task.java
+++ src/Task.java
@@ -1,25 +1,36 @@
 public class Task {
-	
+
 	// For floating Task only the name is required
-	public Task(String taskName){
+	public Task(String taskName) {
 		setName(taskName);
 	}
-	
-	private String name="";
-	private boolean isDone=false;
-	
-	public String getName(){
+
+	private String name = "";
+	private boolean isDone = false;
+	private String description = "";
+
+	public String getName() {
 		return name;
 	}
-	public void setName(String newName){
-		name=newName;
+
+	public void setName(String newName) {
+		name = newName;
 	}
-	
-	public boolean getIsDone(){
+
+	public boolean getIsDone() {
 		return isDone;
 	}
-	public void setIsDone(Boolean doneState){
-		isDone=doneState;
+
+	public void setIsDone(Boolean doneState) {
+		isDone = doneState;
 	}
-	
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String newDescription) {
+		description = newDescription;
+	}
+
 }

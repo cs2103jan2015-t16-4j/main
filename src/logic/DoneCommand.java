8d1c35f298168a0235f138c6e7e5fbdf6b610e09
linzr <noctislin@gmail.com>
2015-03-24 02:01:20 +0800

    Add unique id functionality to Done command.

--- src/logic/DoneCommand.java
+++ src/logic/DoneCommand.java
@@ -4,37 +4,40 @@ import java.util.ArrayList;
 
 import common.Task;
 
-import database.Database;
-
 //@author A0119384Y
 public class DoneCommand extends Command {
 	private final static String TAG_DONE = "#done";
 	private String name;
-	private ArrayList<Task> taskList = Database.getTaskList();
-	private ArrayList<Integer> resultTaskIndexes = new ArrayList<Integer>();
+	int taskId  =-1;
 
 	public DoneCommand(String name) {
-		assert name != null && name != "";
 		this.name = name;
+	}
+	
+	public DoneCommand(int taskId) {
+		this.taskId = taskId;
 	}
 
 	public ArrayList<Task> execute() {
-		for (int index = 0; index < taskList.size(); index++) {
-			if (taskList.get(index).getName().equalsIgnoreCase(name)) {
-				resultTaskIndexes.add(index);
-			}
-		}
-		if (resultTaskIndexes.size() == 1) {
-			taskList.get(resultTaskIndexes.get(0)).addTag(TAG_DONE);
-			ArrayList<Task> r = new ArrayList<Task>();
-			r.add(taskList.get(resultTaskIndexes.get(0)));
-			return r;
-			// TODO: return successful edit message
-		} else {
-			// TODO: return unsuccessful edit message
+		String[] s = {TAG_DONE};
+		if(isDoneWithId()){
+			Command c = new TagCommand(taskId, s);
+			return c.execute();
+		}else if(isDoneWithName()){
+			Command c = new TagCommand(name, s);
+			return c.execute();
+		}else{
 			return null;
 		}
 
+	}
+	
+	private boolean isDoneWithName() {
+		return name!=null && taskId<0;
+	}
+
+	private boolean isDoneWithId() {
+		return name==null && taskId>=0;
 	}
 
 }

5f0beaff0054e38d7259cbf31ebb2bd2dc99b0db
linzr <noctislin@gmail.com>
2015-03-24 02:02:54 +0800

    Add unique id functionality to Tag command.

--- src/logic/TagCommand.java
+++ src/logic/TagCommand.java
@@ -3,46 +3,103 @@ package logic;
 import java.util.ArrayList;
 
 import common.Task;
-
 import database.Database;
 
 //@author A0119384Y
-public class TagCommand extends Command{
+public class TagCommand extends Command {
 	private String name;
+	private int taskId = -1;
 	private ArrayList<String> tags = new ArrayList<String>();
 	private ArrayList<Task> taskList = Database.getTaskList();
 	private ArrayList<Integer> resultTaskIndexes = new ArrayList<Integer>();
-	
-	public TagCommand(String name, String[] tags){
+
+	public TagCommand(String name, String[] tags) {
 		this.name = name;
-		for (String tag: tags){
+		for (String tag : tags) {
+			tag = formatTag(tag);
+			this.tags.add(tag.toLowerCase());
+		}
+	}
+
+	public TagCommand(int id, String[] tags) {
+		this.taskId = id;
+		for (String tag : tags) {
 			tag = formatTag(tag);
 			this.tags.add(tag.toLowerCase());
 		}
 	}
 
 	private String formatTag(String tag) {
-		if(!tag.startsWith("#")){
+		if (!tag.startsWith("#")) {
 			tag = "#" + tag;
 		}
 		return tag;
 	}
-	
-	public ArrayList<Task> execute(){
-		for (int index = 0; index < taskList.size(); index++) {
-			if (taskList.get(index).getName().equalsIgnoreCase(name)) {
-				resultTaskIndexes.add(index);
-			}
+
+	public ArrayList<Task> execute() {
+		if (isTagWithName()) {
+			return tagWithName();
+		} else if (isTagWithId()) {
+			return tagWithId();
+		} else {
+			return null;
 		}
+
+	}
+
+	private boolean isTagWithId() {
+		return taskId >= 0;
+	}
+
+	private boolean isTagWithName() {
+		return name != null;
+	}
+
+	private ArrayList<Task> tagWithName() {
+		searchWithName();
+		return addTag();
+	}
+
+	private ArrayList<Task> tagWithId() {
+		searchWithId();
+		return withIdToWithName(addTag());
+	}
+
+	private ArrayList<Task> addTag() {
 		if (resultTaskIndexes.size() == 1) {
 			taskList.get(resultTaskIndexes.get(0)).addTags(tags);
 			ArrayList<Task> r = new ArrayList<Task>();
 			r.add(taskList.get(resultTaskIndexes.get(0)));
 			return r;
-			//TODO: return successful tag message
 		} else {
-			//TODO: return unsuccessful tag message
 			return null;
+		}
+	}
+
+	private void searchWithId() {
+		for (int index = 0; index < taskList.size(); index++) {
+			if (taskList.get(index).getId() == taskId) {
+				resultTaskIndexes.add(index);
+				break;
+			}
+		}
+	}
+
+	private void searchWithName() {
+		for (int index = 0; index < taskList.size(); index++) {
+			if (taskList.get(index).getName().equalsIgnoreCase(name)) {
+				resultTaskIndexes.add(index);
+			}
+		}
+	}
+	
+	private ArrayList<Task> withIdToWithName(ArrayList<Task> result) {
+		if(result==null){
+			name = String.valueOf(taskId);
+			taskId = -1;
+			return execute();
+		} else {
+			return result;
 		}
 	}
 }

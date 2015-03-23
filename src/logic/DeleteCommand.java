b331b9c26f3c453899b1a423cee6eb78bf0391ec
linzr <noctislin@gmail.com>
2015-03-24 02:01:05 +0800

    Add unique id functionality to Delete command.

--- src/logic/DeleteCommand.java
+++ src/logic/DeleteCommand.java
@@ -8,7 +8,7 @@ import database.Database;
 //@author A0119384Y
 public class DeleteCommand extends Command {
 	private String name, tag;
-	private int taskId;
+	private int taskId = -1;
 	private ArrayList<Task> taskList = Database.getTaskList();
 	private ArrayList<Integer> resultTaskIndexes = new ArrayList<Integer>();
 
@@ -19,7 +19,7 @@ public class DeleteCommand extends Command {
 	}
 
 	public DeleteCommand(int taskId) {
-		assert taskId > 0;
+		assert taskId >= 0;
 		this.taskId = taskId;
 	}
 
@@ -30,85 +30,111 @@ public class DeleteCommand extends Command {
 		this.tag = tag;
 	}
 
+	public DeleteCommand(int taskId, String tag) {
+		assert taskId >= 0;
+		assert tag != null && tag != "";
+		this.taskId = taskId;
+		this.tag = tag;
+	}
+
 	public ArrayList<Task> execute() {
 
 		if (isDeleteTaskWithId()) {
-			return executeWithId();
+			return deleteTaskWithId();
 		} else if (isDeleteTaskWithName()) {
-			return executeWithName();
-		} else if (isDeleteTagWithName()){
-			return executeWithTag();
+			return deleteTaskWithName();
+		} else if (isDeleteTagWithName()) {
+			return deleteTagWithName();
+		} else if (isDeleteTagWithId()){
+			return deleteTagWithId();
 		} else {
-			//return invalid
+			// return invalid
 			return null;
 		}
 	}
 
 	private boolean isDeleteTagWithName() {
-		return name != null && tag == null;
+		return taskId < 0 && name != null && tag != null;
+	}
+
+	private boolean isDeleteTagWithId() {
+		return taskId >= 0 && name == null && tag != null;
 	}
 
 	private boolean isDeleteTaskWithName() {
-		return tag == null;
+		return taskId < 0 && name != null && tag == null;
 	}
 
 	private boolean isDeleteTaskWithId() {
-		return name == null;
+		return taskId >= 0 && name == null && tag == null;
 	}
 
-	private ArrayList<Task> executeWithId() {
+	private ArrayList<Task> deleteTaskWithId() {
+		searchWithId();
+		return withIdToWithName(deleteTask());
+	}
+
+	private ArrayList<Task> deleteTaskWithName() {
+		searchWithName();
+		return deleteTask();
+	}
+
+	private ArrayList<Task> deleteTagWithId() {
+		searchWithId();
+		return deleteTag();
+	}
+
+	private ArrayList<Task> deleteTagWithName() {
+		searchWithName();
+		return deleteTag();
+	}
+
+	private void searchWithId() {
 		for (int index = 0; index < taskList.size(); index++) {
 			if (taskList.get(index).getId() == taskId) {
 				resultTaskIndexes.add(index);
 				break;
 			}
 		}
-		assert (resultTaskIndexes.size() == 0 || resultTaskIndexes.size() == 1);
-		if (resultTaskIndexes.size() == 1) {
-			ArrayList<Task> r = new ArrayList<Task>();
-			r.add(taskList.get(resultTaskIndexes.get(0)));
-			taskList.remove(taskList.get(resultTaskIndexes.get(0)));
-			return r;
-			// TODO: return successful removal message
-		} else {
-			name = String.valueOf(taskId);
-			return executeWithName();
-		}
 	}
 
-	private ArrayList<Task> executeWithName() {
+	private void searchWithName() {
 		for (int index = 0; index < taskList.size(); index++) {
 			if (taskList.get(index).getName().equalsIgnoreCase(name)) {
 				resultTaskIndexes.add(index);
 			}
 		}
+	}
+
+	private ArrayList<Task> deleteTask() {
 		if (resultTaskIndexes.size() == 1) {
 			ArrayList<Task> r = new ArrayList<Task>();
 			r.add(taskList.get(resultTaskIndexes.get(0)));
 			taskList.remove(taskList.get(resultTaskIndexes.get(0)));
 			return r;
-			// TODO: return successful removal message
 		} else {
-			// TODO: return unsuccessful removal message
 			return null;
 		}
 	}
 
-	private ArrayList<Task> executeWithTag() {
-		for (int index = 0; index < taskList.size(); index++) {
-			if (taskList.get(index).getName().equalsIgnoreCase(name)) {
-				resultTaskIndexes.add(index);
-			}
-		}
+	private ArrayList<Task> deleteTag() {
 		if (resultTaskIndexes.size() == 1) {
 			ArrayList<Task> r = new ArrayList<Task>();
 			r.add(taskList.get(resultTaskIndexes.get(0)));
 			taskList.get(resultTaskIndexes.get(0)).removeTag(tag);
 			return r;
-			// TODO: return successful removal message
 		} else {
 			return null;
-			// TODO: return unsuccessful removal message
+		}
+	}
+
+	private ArrayList<Task> withIdToWithName(ArrayList<Task> result) {
+		if(result==null){
+			name = String.valueOf(taskId);
+			taskId = -1;
+			return execute();
+		} else {
+			return result;
 		}
 	}
 }

ac8b29132b002cb342c080a062ab2ac39415b189
Joan <joan.tan.xy@gmail.com>
2015-03-24 06:50:01 +0800

    Implemented hashcode and equals method for task class

--- src/common/Task.java
+++ src/common/Task.java
@@ -95,4 +95,64 @@ public class Task {
 	public boolean isContainedTag(String tag) {
 		return tags.contains(tag);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((endTime == null) ? 0 : endTime.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((startTime == null) ? 0 : startTime.hashCode());
+		result = prime * result + ((tags == null) ? 0 : tags.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj){
+			return true;
+		}
+		if (obj == null){
+			return false;
+		}
+		if (getClass() != obj.getClass()){
+			return false;
+		}
+		
+		Task other = (Task) obj;
+		
+		if (endTime == null) {
+			if (other.endTime != null){
+				return false;
+			}
+		} else if (!endTime.equals(other.endTime)){
+			return false;
+		}
+		if (id != other.id){
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null){
+				return false;
+			}
+		} else if (!name.equals(other.name)){
+			return false;
+		}
+		if (startTime == null) {
+			if (other.startTime != null){
+				return false;
+			}
+		} else if (!startTime.equals(other.startTime)){
+			return false;
+		}
+		if (tags == null) {
+			if (other.tags != null){
+				return false;
+			}
+		} else if (!tags.equals(other.tags)){
+			return false;
+		}
+		return true;
+	}
 }

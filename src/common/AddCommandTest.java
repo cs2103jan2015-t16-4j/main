05accbfc0b296052a8aa11e44a6a0bbbb9e16aaa
linzr <noctislin@gmail.com>
2015-03-26 16:38:44 +0800

    Minor changes in AddCommandTest.

--- src/common/AddCommandTest.java
+++ src/common/AddCommandTest.java
@@ -16,5 +16,20 @@ public class AddCommandTest {
 		ArrayList<Task> result = a.execute();
 		assertEquals("update manual",result.get(0).getName());
 	}
+	
+	@Test
+	public void addNullTest() {
+		/* This is a boundary case for the null value task name*/
+		AddCommand a = new AddCommand(null, null ,null);
+		ArrayList<Task> result = a.execute();
+		assertEquals(null, result.get(0).getName());
+	}	
 
+	@Test
+	public void addEmptyNameTest() {
+		/* This is a boundary case for the empty name task name*/
+		AddCommand a = new AddCommand("", null ,null);
+		ArrayList<Task> result = a.execute();
+		assertEquals("", result.get(0).getName());
+	}
 }

50ba2fdb8bc0cd052eb903458c4aa6e9bdde4645
linzr <noctislin@gmail.com>
2015-03-31 23:40:14 +0800

    Add flexibility to command. Fix #17.

--- src/logic/Command.java
+++ src/logic/Command.java
@@ -6,26 +6,42 @@ import common.Task;
 
 //@author A0119384Y
 public abstract class Command {
+	private static final String[] KEYWORD_SAVE = { "save" };
+	private static final String[] KEYWORD_ADD = { "add","create" };
+	private static final String[] KEYWORD_DELETE = { "delete","remove" };
+	private static final String[] KEYWORD_EDIT = { "edit","update" };
+	private static final String[] KEYWORD_DISPLAY = { "display","search" };
+	private static final String[] KEYWORD_TAG = { "tag","mark" };
+	private static final String[] KEYWORD_DONE = { "done" };
+	private static final String[] KEYWORD_UNDO = { "undo" };
+	private static final String[] KEYWORD_SETPATH = { "setpath","path" };
+	private static final String[] KEYWORD_INVALID = { "invalid" };
+	private static final String[] KEYWORD_EXIT = { "exit" };
+	private static final String[] KEYWORD_HELP = { "help" };
+
 	public static enum CommandType {
-		SAVE("save"), ADD("add"), DELETE("delete"), EDIT("edit"), DISPLAY("display"), TAG(
-				"tag"), DONE("done"), UNDO("undo"), SETPATH("setpath"), INVALID(
-				"invalid"), EXIT("exit"), HELP("help");
+		SAVE(KEYWORD_SAVE), ADD(KEYWORD_ADD), DELETE(KEYWORD_DELETE), EDIT(
+				KEYWORD_EDIT), DISPLAY(KEYWORD_DISPLAY), TAG(KEYWORD_TAG), DONE(
+				KEYWORD_DONE), UNDO(KEYWORD_UNDO), SETPATH(KEYWORD_SETPATH), INVALID(
+				KEYWORD_INVALID), EXIT(KEYWORD_EXIT), HELP(KEYWORD_HELP);
 
-		private String commandType;
+		private String[] commandTypeDescs;
 
-		CommandType(String commandType) {
-			this.commandType = commandType;
+		CommandType(String[] commandType) {
+			this.commandTypeDescs = commandType;
 		}
 
 		public String getText() {
-			return this.commandType;
+			return this.commandTypeDescs[0];
 		}
 
 		public static CommandType fromString(String text) {
 			if (text != null) {
 				for (CommandType b : CommandType.values()) {
-					if (text.equalsIgnoreCase(b.commandType)) {
-						return b;
+					for (String desc : b.commandTypeDescs) {
+						if (text.equalsIgnoreCase(desc)) {
+							return b;
+						}
 					}
 				}
 			}
